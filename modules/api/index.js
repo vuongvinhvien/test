926dfe5979e26598851aeb309d227b5660c129cf
vuong vinh vien <vuongvinhvien@gmail.com>
2020-07-28 18:47:34 +0700

    aa

--- modules/api/index.js
+++ modules/api/index.js
@@ -3,9 +3,9 @@ module.exports = function (moduleOptions) {
   console.warn('create module ....middleware')
   if (this.options.dev || this.options._start) {
     console.warn('okkkkkkkkkkkkkk')
-    console.warn([this.options.dev])
+    // console.warn([this.options.dev])
     // console.warn([this.options._env.NODE_ENV])
-    console.warn([moduleOptions])
+    // console.warn([moduleOptions])
     // console.warn([this.options._start])
     // console.log([this.options])
     this.addServerMiddleware('~/api/')
